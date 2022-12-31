86f0b46a76842aa4f6e2d1a3ec15bb2a2762b475
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-30 23:55:57 -0800

    Added display table

--- subnet-calculator/src/App.js
+++ subnet-calculator/src/App.js
@@ -6,17 +6,34 @@ import { useState } from "react";
 const App = () => {
   const [ipAddress, setIpAddress] = useState([0, 0, 0, 0]);
   const [slashValue, setSlashValue] = useState(24);
+  const [networkAddress, setNetworkAddress] = useState("");
+  const [addressClass, setAddressClass] = useState("");
+  const [firstAddress, setFirstAddress] = useState("");
+  const [lastAddress, setLastAddress] = useState("");
+  const [broadcastAddress, setBroadcastAddress] = useState("");
+  const [nextNetwork, setNextNetwork] = useState("");
+  const [availableHosts, setAvailableHosts] = useState("");
+  const [numberSubnets, setNumberSubnets] = useState("");
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log("whatever");
+    // console.log(ipAddress);
+    setNetworkAddress(ipAddress);
+    setAddressClass("A");
+    setFirstAddress(ipAddress);
+    setLastAddress(ipAddress);
+    setNextNetwork(ipAddress);
+    setBroadcastAddress(ipAddress);
+    setNetworkAddress(ipAddress);
+    setAvailableHosts(512);
+    setNumberSubnets(256);
   };
 
   const updateIpAddress = (e, quartetIndex) => {
     let newIpAddress = [...ipAddress];
     newIpAddress[quartetIndex] = parseInt(e.currentTarget.value);
     setIpAddress(newIpAddress);
-    console.log(newIpAddress);
+    // console.log(newIpAddress);
   };
 
   const updateSlashValue = (e) => {
@@ -26,7 +43,7 @@ const App = () => {
   };
 
   return (
-    <div className="container text-center" style={{ minWidth: "450px" }}>
+    <div className="container text-center mt-2" style={{ minWidth: "450px" }}>
       <h1>Subnet Calculator</h1>
       <div className="row">
         <div className="col-8 d-flex justify-content-around">
@@ -134,6 +151,50 @@ const App = () => {
             Do
           </button>
         </div>
+      </div>
+      <div className="row">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col" className="col-6"></th>
+              <th scope="col" className="col-6"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table-primary">
+              <th scope="row">NETWORK ADDRESS</th>
+              <td>{networkAddress}</td>
+            </tr>
+            <tr className="table-default">
+              <th scope="row">FIRST ADDRESS</th>
+              <td>{firstAddress}</td>
+            </tr>
+            <tr className="table-primary">
+              <th scope="row">LAST HOST ADDRESS</th>
+              <td>{lastAddress}</td>
+            </tr>
+            <tr className="table-default">
+              <th scope="row">BROADCAST ADDRESS</th>
+              <td>{broadcastAddress}</td>
+            </tr>
+            <tr className="table-primary">
+              <th scope="row">NEXT NETWORK</th>
+              <td>{nextNetwork}</td>
+            </tr>
+            <tr className="table-default">
+              <th scope="row">NUMBER OF AVAILABLE HOST ADDRESSES</th>
+              <td>{availableHosts}</td>
+            </tr>
+            <tr className="table-primary">
+              <th scope="row">CLASS</th>
+              <td>{addressClass}</td>
+            </tr>
+            <tr className="table-default">
+              <th scope="row">NUMBER OF SUBNETS</th>
+              <td>{numberSubnets}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
